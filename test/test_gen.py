efebc52702fb65a0301f33c8729db3a2270d8449
AliceBalfanz <alicja.balfanz@brockmann-consult.de>
2019-07-30 10:07:15 +0200

    adjusting tests to changes in xcube and travis build for xcube-gen-bc

--- test/test_gen.py
+++ test/test_gen.py
@@ -23,37 +23,34 @@ class SnapProcessTest(unittest.TestCase):
 
     # noinspection PyMethodMayBeStatic
     def test_process_inputs_single(self):
-        path, status = process_inputs_wrapper(input_files=[get_inputdata_file('O_L2_0001_SNS_2017105100139_v1.0.nc')],
-                                              output_name='l2c-single',
-                                              output_writer='netcdf4',
-                                              append_mode=False)
+        status = process_inputs_wrapper(input_path=[get_inputdata_file('O_L2_0001_SNS_2017105100139_v1.0.nc')],
+                                        output_path='l2c-single.nc',
+                                        output_writer='netcdf4',
+                                        append_mode=False)
         self.assertEqual(True, status)
-        self.assertEqual(os.path.join('.', 'l2c-single.nc'), path)
 
     def test_process_inputs_append_multiple_nc(self):
-        path, status = process_inputs_wrapper(input_files=[get_inputdata_file('O_L2_0001_SNS_*_v1.0.nc')],
-                                              output_name='l2c',
-                                              output_writer='netcdf4',
-                                              append_mode=True)
+        status = process_inputs_wrapper(input_path=[get_inputdata_file('O_L2_0001_SNS_*_v1.0.nc')],
+                                        output_path='l2c.nc',
+                                        output_writer='netcdf4',
+                                        append_mode=True)
         self.assertEqual(True, status)
-        self.assertEqual(os.path.join('.', 'l2c.nc'), path)
 
     def test_process_inputs_append_multiple_zarr(self):
-        path, status = process_inputs_wrapper(input_files=[get_inputdata_file('O_L2_0001_SNS_*_v1.0.nc')],
-                                              output_name='l2c',
-                                              output_writer='zarr',
-                                              append_mode=True)
+        status = process_inputs_wrapper(input_path=[get_inputdata_file('O_L2_0001_SNS_*_v1.0.nc')],
+                                        output_path='l2c.zarr',
+                                        output_writer='zarr',
+                                        append_mode=True)
         self.assertEqual(True, status)
-        self.assertEqual(os.path.join('.', 'l2c.zarr'), path)
 
 
 # noinspection PyShadowingBuiltins
-def process_inputs_wrapper(input_files=None,
-                           output_name=None,
+def process_inputs_wrapper(input_path=None,
+                           output_path=None,
                            output_writer='netcdf4',
                            append_mode=False):
-    return gen_cube(input_files=input_files, input_processor='snap-olci-highroc-l2', output_region=(0., 50., 5., 52.5),
-                    output_size=(2000, 1000), output_resampling='Nearest', output_dir='.', output_name=output_name,
+    return gen_cube(input_paths=input_path, input_processor='snap-olci-highroc-l2', output_region=(0., 50., 5., 52.5),
+                    output_size=(2000, 1000), output_resampling='Nearest', output_path=output_path,
                     output_writer=output_writer,
                     output_variables=[('conc_chl', None), ('conc_tsm', None), ('kd489', None)], append_mode=append_mode,
                     dry_run=False, monitor=None)
