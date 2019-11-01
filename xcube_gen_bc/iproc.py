6084fd725ccbe430c88867544b13856ac30d634c
Norman Fomferra <norman.fomferra@gmail.com>
2019-11-01 12:50:49 +0100

    Removed redundancy between extension metadata and metadata provided by classes --> new ExtensionComponent base class

--- xcube_gen_bc/iproc.py
+++ xcube_gen_bc/iproc.py
@@ -24,10 +24,10 @@ from typing import Tuple
 
 import numpy as np
 import xarray as xr
+
 from xcube.api.gen.iproc import XYInputProcessor, ReprojectionInfo
 from xcube.util.constants import CRS_WKT_EPSG_4326
 from xcube.util.timecoord import to_time_in_days_since_1970
-
 from .transexpr import translate_snap_expr_attributes
 from .vectorize import vectorize_wavebands, new_band_coord_var
 
@@ -37,7 +37,8 @@ class SnapNetcdfInputProcessor(XYInputProcessor, metaclass=ABCMeta):
     Input processor for SNAP L2 NetCDF inputs.
     """
 
-    def __init__(self):
+    def __init__(self, name: str):
+        super().__init__(name)
         self.xy_gcp_step = None
 
     @property
@@ -99,25 +100,15 @@ class SnapOlciHighrocL2InputProcessor(SnapNetcdfInputProcessor):
     Input processor for SNAP Sentinel-3 OLCI HIGHROC Level-2 NetCDF inputs.
     """
 
-    @property
-    def name(self) -> str:
-        return 'snap-olci-highroc-l2'
-
-    @property
-    def description(self) -> str:
-        return 'SNAP Sentinel-3 OLCI HIGHROC Level-2 NetCDF inputs'
+    def __init__(self):
+        super().__init__('snap-olci-highroc-l2')
 
 
 # noinspection PyAbstractClass
 class SnapOlciCyanoAlertL2InputProcessor(SnapNetcdfInputProcessor):
     """
-    Input processor for SNAP Sentinel-3 OLCI HIGHROC Level-2 NetCDF inputs.
+    Input processor for SNAP Sentinel-3 OLCI CyanoAlert Level-2 NetCDF inputs.
     """
 
-    @property
-    def name(self) -> str:
-        return 'snap-olci-cyanoalert-l2'
-
-    @property
-    def description(self) -> str:
-        return 'SNAP Sentinel-3 OLCI CyanoAlert Level-2 NetCDF inputs'
+    def __init__(self):
+        super().__init__('snap-olci-cyanoalert-l2')
