c968001b4990f034a1519f6923774bd82f9ed8e7
Norman Fomferra <norman.fomferra@gmail.com>
2019-11-05 12:03:28 +0100

    fixed test

--- test/test_plugin.py
+++ test/test_plugin.py
@@ -1,11 +1,14 @@
 import unittest
 
-from xcube_gen_bc import init_plugin
+from xcube.util import extension
+from xcube_gen_bc.plugin import init_plugin
 
 
 class SnapPluginTest(unittest.TestCase):
 
     # noinspection PyMethodMayBeStatic
     def test_init_plugin(self):
-        # Smoke test
-        init_plugin()
+        ext_reg = extension.ExtensionRegistry()
+        init_plugin(ext_reg)
+        self.assertTrue(ext_reg.has_extension('xcube.core.gen.iproc', 'snap-olci-highroc-l2'))
+        self.assertTrue(ext_reg.has_extension('xcube.core.gen.iproc', 'snap-olci-cyanoalert-l2'))
